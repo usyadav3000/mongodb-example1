527a6b988d5926ac470dc57f74cf3195002cdced
umashankar yadav <usyadav008@gmail.com>
2020-05-17 17:47:45 +0530

    deleted 1 item

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo=db.db('myapp2');
-  var mysort= {name:1};
-  dbo.collection("customers").find().sort(mysort).toArray(function(err,result){
+  var myquery= {name:'mani'};
+  dbo.collection("customers").deleteOne(myquery,function(err,result){
       if(err) throw err;
-    console.log(result);
+    console.log("1 documents deleted!");
     db.close();
   });
   
