5c8b1f5890ced874f848b2f80501dba6a5026853
umashankar yadav <usyadav008@gmail.com>
2020-05-17 17:34:57 +0530

    query or filter

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo=db.db('myapp2');
-  
-  dbo.collection("customers").find({}).toArray(function(err,result){
+  var query= {id:"1004"};
+  dbo.collection("customers").find(query).toArray(function(err,result){
       if(err) throw err;
     console.log(result);
     db.close();
