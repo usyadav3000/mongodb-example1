1080a736cb436434c16c32fc8d0f484c2298582b
umashankar yadav <usyadav008@gmail.com>
2020-05-17 17:29:50 +0530

    find all documents

--- index.js
+++ index.js
@@ -4,17 +4,10 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo=db.db('myapp2');
-  var myobj=[
-      {name:"umashankar",age:"22",id:"1001"},
-      {name:"aditya",age:"21",id:"1002"},
-      {name:"mani",age:"23",id:"1003"},
-      {name:"ashay",age:"20",id:"1004"},
-      {name:"vijay",age:"43",id:"1005"}
-    
-    ];
-  dbo.collection("customers").insertMany(myobj,function(err,res){
+  
+  dbo.collection("customers").find({}).toArray(function(err,result){
       if(err) throw err;
-    console.log("nomber of document inserted: "+ res.insertedCount);
+    console.log(result);
     db.close();
   });
   
