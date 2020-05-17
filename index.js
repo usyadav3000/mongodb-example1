7001e2b51d86b6e56e92385dfdbb78e8518e71b2
umashankar yadav <usyadav008@gmail.com>
2020-05-17 17:24:37 +0530

    insrted more item

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo=db.db('myapp2');
-  var myobj={name:"umashankar",age:"22",id:"1001"};
-  dbo.collection("customers").insertOne(myobj,function(err,res){
+  var myobj=[
+      {name:"umashankar",age:"22",id:"1001"},
+      {name:"aditya",age:"21",id:"1002"},
+      {name:"mani",age:"23",id:"1003"},
+      {name:"ashay",age:"20",id:"1004"},
+      {name:"vijay",age:"43",id:"1005"}
+    
+    ];
+  dbo.collection("customers").insertMany(myobj,function(err,res){
       if(err) throw err;
-    console.log("1 document inserted!");
+    console.log("nomber of document inserted: "+ res.insertedCount);
     db.close();
   });
   
