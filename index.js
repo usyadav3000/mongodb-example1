5b4ed4cf69944c2b712546eff105e328e6a7b062
umashankar yadav <usyadav008@gmail.com>
2020-05-17 17:42:14 +0530

    sort incresing or decreasing order

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo=db.db('myapp2');
-  var query= {id:"1004"};
-  dbo.collection("customers").find(query).toArray(function(err,result){
+  var mysort= {name:1};
+  dbo.collection("customers").find().sort(mysort).toArray(function(err,result){
       if(err) throw err;
     console.log(result);
     db.close();
