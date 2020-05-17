0be0258754810a01ac367b4e6cff700429d93c80
umashankar yadav <usyadav008@gmail.com>
2020-05-17 17:14:09 +0530

    insted item in mongo

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo=db.db('myapp2');
-  dbo.createCollection("customers",function(err,res){
+  var myobj={name:"umashankar",age:"22",id:"1001"};
+  dbo.collection("customers").insertOne(myobj,function(err,res){
       if(err) throw err;
-      console.log("Collection created!");
-      db.close();
+    console.log("1 document inserted!");
+    db.close();
   });
   
 });
